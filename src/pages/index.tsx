77cbc3d3391d07d12f385cf5c882509e627c6ad3
j-dax <67487509+j-dax@users.noreply.github.com>
2023-06-16 13:45:59 -0700

    posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
-// import { api } from "~/utils/api";
+import { api } from "~/utils/api";
 
 export default function Home() {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
   const user = useUser();
   return (
     <>
@@ -15,6 +15,11 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {user.isSignedIn && <SignOutButton />}
         {!user.isSignedIn && <SignInButton />}
+        <div>
+          {data?.map(post => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
